5b7908a7aaed21f2a2ed9907e51d1cd2602b9f58
User <user@Nishads-MacBook-Air.local>
2022-02-04 20:38:52 +0530

    django paymnt

--- setup/afl_events/models.py
+++ setup/afl_events/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from decimal import Decimal
+
 
 # Create your models here.
 # class User(models.Model):
@@ -20,3 +22,24 @@ class Event(models.Model):
     date = models.DateField()
     time = models.TimeField()
     duration = models.IntegerField()
+
+
+
+
+from decimal import Decimal
+
+from payments import PurchasedItem
+from payments.models import BasePayment
+
+class Payment(BasePayment):
+
+    def get_failure_url(self):
+        return 'http://localhost:8000/failure/'
+
+    def get_success_url(self):
+        return 'http://localhost:8000/success/'
+
+    def get_purchased_items(self):
+        # you'll probably want to retrieve these from an associated order
+        yield PurchasedItem(name='The Hound of the Baskervilles', sku='BSKV',
+                            quantity=9, price=Decimal(10), currency='USD')
--- setup/afl_events/urls.py
+++ setup/afl_events/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from django.contrib import admin
 from afl_events.views import addEvent,PayAmount
+# from afl_events.checkout import create_checkout_session
+# from django.conf.urls import include, path
 
 app_name = 'afl_events'
 
@@ -27,18 +29,7 @@ urlpatterns = [
     url(r'^user_home',views.user_home,name='user_home'),
 
     url(r'^user_event',views.user_event,name='user_event'),
-    # url(r'^ground_booking',views.ground_booking,name='ground_booking'),
-    # url(r'^db_ground_booking',views.db_ground_booking,name='db_ground_booking'),
-
-    # url(r'^admin_booking',views.admin_booking,name='admin_booking'),
-    # url(r'^admin_event',views.admin_event,name='admin_event'),
-
-    # url(r'^update_event/(?P<id>\d+)/$',views.update_event,name='update_event'),
-    # url(r'^db_update_event/(?P<id>\d+)/$',views.db_update_event,name='db_update_event'),
-    # url(r'^db_delete_event/(?P<id>\d+)/$',views.db_delete_event,name='db_delete_event'),
-
-    # url(r'^add_event',views.add_event,name='add_event'),
-    # url(r'^db_add_event',views.db_add_event,name='db_add_event'),
+   
 
     url(r'^user_logout',views.user_logout,name='user_logout'),
     # url(r'^admin_logout',views.admin_logout,name='admin_logout'),
@@ -64,5 +55,9 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
     path('post/ajax/add_event', addEvent, name = "add_event"),
-    url(r'^add-payment/(?P<e_id>\d+)/$', PayAmount.as_view(), name='pay_amount'),
+    # url(r'^checkout/(?P<e_id>\d+)/$', views.Payment_details.as_view(), name='checkout_page'),
+    path('checkout/', views.Payment_details.as_view(), name='checkout_page'),
+
+    # url(r'^checkout/(?P<e_id>\d+)/$',views.Payment_details.as_view(),name='checkout_page'),
+    # path('payments/', include('payments.urls')),
 ]
